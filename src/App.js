fbf1e68ad5a4035991593dad8f77f8f8244d88f2
subrjak <bogdangirichev125@gmail.com>
2026-06-11 19:49:55 +0500

    обновил

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
-            path="/profile"
+            path="/profile" 
             element={
               <PrivateRoute>
                 <ProfilePage />
